3814a0e412ddcebd126c5f8260b5c79f0acc76a5
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-10-27 00:29:34 +0200

    fix conf

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_key # Allow read access to the private key
-ssh-add $TRAVIS_BUILD_DIR/.travis/deploy_key # Add the private key to SSH
+chmod 600 .travis/deploy_key # Allow read access to the private key
+ssh-add .travis/deploy_key # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://git@$SSH_SERVER:$SSH_PORT$DEPLOY_PATH
-git push deploy master
+git push deploy dev
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh $SSH_USER@$SSH_SERVER -p $SSH_PORT <<EOF
