1476df2aafd791c29909495128f43471bb8346b8
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-10-28 11:36:58 +0200

    fix travis

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -5,12 +5,13 @@ chmod 600 .travis/deploy_key # Allow read access to the private key
 ssh-add .travis/deploy_key # Add the private key to SSH
 
 git config --global push.default matching
-GIT_SSH_COMMAND="ssh -p $SSH_PORT -o StrictHostKeyChecking=no" git remote add deploy ssh://git@$SSH_SERVER:$DEPLOY_PATH
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git remote add deploy ssh://git@$SSH_SERVER:$SSH_PORT$DEPLOY_PATH
 git push deploy dev
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no <<EOF
-  cd $DEPLOY_PATH
+  zsh
+  cd $DEPLOY_PATHl
   pm2 stop $PROJECT
   pm2 delete $PROJECT
   pm2 flush
