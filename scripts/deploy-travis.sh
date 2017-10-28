42dbf7b5897e710f837544ea464914a4ce45bc9b
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-10-28 12:52:14 +0200

    test

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -4,12 +4,13 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy_key # Allow read access to the private key
 ssh-add .travis/deploy_key # Add the private key to SSH
 
-git config --global push.default matching
-GIT_SSH_COMMAND="ssh -p $SSH_PORT -o StrictHostKeyChecking=no" git remote add deploy ssh://git@$SSH_SERVER:$DEPLOY_PATH
-git push deploy dev
+# git config --global push.default matching
+# GIT_SSH_COMMAND="ssh -p $SSH_PORT -o StrictHostKeyChecking=no" git remote add deploy ssh://git@$SSH_SERVER:$DEPLOY_PATH
+# git push deploy dev
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no <<EOF
+  zsh
   cd $DEPLOY_PATH
   pm2 stop $PROJECT
   pm2 delete $PROJECT
