1eb3228d8aed659737dd4958b9711c8afacf65aa
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-10-29 11:44:45 +0100

    test

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -5,7 +5,8 @@ chmod 600 .travis/deploy_key # Allow read access to the private key
 ssh-add .travis/deploy_key # Add the private key to SSH
 
 ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<'EOT'
-  cd $DEPLOY_PATH
+  echo $DEPLOY_PATH  >> ./test.log
+  echo '$DEPLOY_PATH'  >> ./test1.log
   echo "This logs to where I want, but using echo" >> ./deploy.log
   node -v >> ./deploy.log
 EOT
