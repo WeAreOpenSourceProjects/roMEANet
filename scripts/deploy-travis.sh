d0505a7fe9125d79f5a6c1f973344dfae4c13266
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-11-02 17:17:56 +0100

    test

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_key # Allow read access to the private key
+ssh-add $TRAVIS_BUILD_DIR/.travis/deploy_key # Add the private key to SSH
+
 ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<EOT
   echo "$(date -u) Start Deploy '${PROJECT}'"  >> ./deploy.log
   cd '${DEPLOY_PATH}'
+  npm install
+  gulp WeaosProd
   pm2 stop '${PROJECT}'
   pm2 delete '${PROJECT}'
   pm2 flush
