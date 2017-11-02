21a342c9c63c5a182d93b228d43bbb30e6f9b6e0
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-11-02 17:07:08 +0100

    test

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<EOT
-  zsh
-  echo "$(date -u) Start Deploy '${PROJECT}'"  >> ../deploy.log
+  echo "$(date -u) Start Deploy '${PROJECT}'"  >> ./deploy.log
   cd '${DEPLOY_PATH}'
   pm2 stop '${PROJECT}'
   pm2 delete '${PROJECT}'
   pm2 flush
   NODE_ENV=production pm2 start server.js --name '${PROJECT}'
-  echo "$(date -u) End Deploy '${PROJECT}'"  >> ../deploy.log
+  cd
+  echo "$(date -u) End Deploy '${PROJECT}'"  >> ./deploy.log
   exit
 EOT
 )"
