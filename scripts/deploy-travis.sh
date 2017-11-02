e6a70c00c82accda7a499d0ca50d7ea8a055c8ed
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-11-02 11:18:09 +0100

    test

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -5,8 +5,14 @@ chmod 600 .travis/deploy_key # Allow read access to the private key
 ssh-add .travis/deploy_key # Add the private key to SSH
 
 ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<EOT
-  echo " toto '${DEPLOY_PATH}'"  >> ./test1.log
-  echo "This logs to where I want, but using echo" >> ./deploy.log
-  node -v >> ./deploy.log
+  zsh
+  cd '${$DEPLOY_PATH}'
+  pm2 stop '${$PROJECT}'
+  pm2 delete '${$PROJECT}'
+  pm2 flush
+  npm install
+  gulp WeaosProd
+  NODE_ENV=production pm2 start server.js --name '${$PROJECT}'
+  echo "End Deploy '${$PROJECT}'"  >> ../deploy.log
 EOT
 )"
