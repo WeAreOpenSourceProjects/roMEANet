ad06d29e05e23912d17c249509f7e01cd66d86ee
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-11-02 19:50:43 +0100

    last try

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -4,17 +4,16 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_key # Allow read access to the private key
 ssh-add $TRAVIS_BUILD_DIR/.travis/deploy_key # Add the private key to SSH
 
-ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<EOT
-  echo "$(date -u) Start Deploy '${PROJECT}'"  >> ./deploy.log &&
-  cd '${DEPLOY_PATH}' &&
-  npm install &&
-  gulp WeaosProd &&
-  pm2 stop '${PROJECT}' &&
-  pm2 delete '${PROJECT}' &&
+ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no /bin/bash <<EOT
+  echo "$(date -u) Start Deploy $PROJECT"  >> ./deploy.log
+  cd $DEPLOY_PATH
+  npm install
+  gulp WeaosProd
+  pm2 stop $PROJECT
+  pm2 delete $PROJECT
   pm2 flush &&
-  NODE_ENV=production pm2 start server.js --name '${PROJECT}' &&
+  NODE_ENV=production pm2 start server.js --name PROJECT
   cd &&
-  echo "$(date -u) End Deploy '${PROJECT}'"  >> ./deploy.log &&
+  echo "$(date -u) End Deploy $PROJECT"  >> ./deploy.log
   exit
 EOT
-)"
