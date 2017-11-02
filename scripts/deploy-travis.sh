3a874a2165e6b7e20d6c04440af6ea1ddb6e0a7e
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-11-02 11:40:18 +0100

    clean

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -6,13 +6,13 @@ ssh-add .travis/deploy_key # Add the private key to SSH
 
 ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<EOT
   zsh
-  cd '${$DEPLOY_PATH}'
-  pm2 stop '${$PROJECT}'
-  pm2 delete '${$PROJECT}'
+  cd '${DEPLOY_PATH}'
+  pm2 stop '${PROJECT}'
+  pm2 delete '${PROJECT}'
   pm2 flush
   npm install
   gulp WeaosProd
-  NODE_ENV=production pm2 start server.js --name '${$PROJECT}'
-  echo "End Deploy '${$PROJECT}'"  >> ../deploy.log
+  NODE_ENV=production pm2 start server.js --name '${PROJECT}'
+  echo "End Deploy '${PROJECT}'"  >> ../deploy.log
 EOT
 )"
