5d48e5e8a5d5fd9494aeb5ef1f7f21b16dbfad84
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-11-02 11:05:07 +0100

    test

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -4,8 +4,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy_key # Allow read access to the private key
 ssh-add .travis/deploy_key # Add the private key to SSH
 
-ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<'EOT'
-  echo $DEPLOY_PATH  >> ./test.log
+ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<EOT
   echo " toto '${DEPLOY_PATH}'"  >> ./test1.log
   echo "This logs to where I want, but using echo" >> ./deploy.log
   node -v >> ./deploy.log
